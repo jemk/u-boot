8a044839fae857028bb48e7d0685b1096b979eae
Andre Przywara <andre.przywara@arm.com>
2016-11-11 00:34:14 +0000

    sunxi: H5/SPL: add 32-bit SPL config with stub DRAM init
    Introduce a 32-bit SPL defconfig for Allwinner H5 boards. This is needed
    to link it with the 32-bit libdram later.
    In preparation for that separate the existing H3 DRAM build from the
    shared H3 Kconfig symbol and introduce a separate DRAM setup file for
    the H5, which is just an empty stub for now.
    
    Signed-off-by: Andre Przywara <andre.przywara@arm.com>

--- arch/arm/include/asm/arch-sunxi/dram.h
+++ arch/arm/include/asm/arch-sunxi/dram.h
@@ -24,10 +24,12 @@
 #include <asm/arch/dram_sun8i_a33.h>
 #elif defined(CONFIG_MACH_SUN8I_A83T)
 #include <asm/arch/dram_sun8i_a83t.h>
-#elif defined(CONFIG_MACH_SUN8I_H3)
+#elif defined(CONFIG_MACH_SUN8I_H3_32)
 #include <asm/arch/dram_sun8i_h3.h>
 #elif defined(CONFIG_MACH_SUN9I)
 #include <asm/arch/dram_sun9i.h>
+#elif defined(CONFIG_MACH_SUN50I_H5)
+#include <asm/arch/dram_libdram.h>
 #else
 #include <asm/arch/dram_sun4i.h>
 #endif
